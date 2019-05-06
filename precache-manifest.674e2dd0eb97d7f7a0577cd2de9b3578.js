d795bb958232578779c78dccca915553d402de85
MaxDudko <max2410zp@gmail.com>
2019-05-06 19:59:47 +0300

    Updates

---
+++ precache-manifest.674e2dd0eb97d7f7a0577cd2de9b3578.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5536fe22281047a23c85800b7abed379",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "52daf607b507678b52be",
+    "url": "/Tic-Tac-Toe/static/css/main.6de18e7e.chunk.css"
+  },
+  {
+    "revision": "9a5c0cc5b7b5942488d9",
+    "url": "/Tic-Tac-Toe/static/js/2.56cb80cd.chunk.js"
+  },
+  {
+    "revision": "52daf607b507678b52be",
+    "url": "/Tic-Tac-Toe/static/js/main.e4b6825d.chunk.js"
+  },
+  {
+    "revision": "10779627d94960e7b271",
+    "url": "/Tic-Tac-Toe/static/js/runtime~main.adeb2bb3.js"
+  }
+]);
